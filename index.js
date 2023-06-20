780a60dbf6bde03b7e06ac661cd3606c0f95e41a
michellemwangi01 <michellemwangim@gmail.com>
2023-06-20 11:00:43 +0300

    COMPLETED

--- index.js
+++ index.js
@@ -11,6 +11,39 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// const titleCased = () => {
+//   tutorials.forEach((sentence)=>{
+//     const words = sentence.split(' ');
+//     console.log(words);
+//     // tutorials.push(words.map((word)=>{
+//     //   word.charAt(0).toUpperCase + word.slice(1);
+//     // })); 
+//   })
+//   //return tutorials
+// }
+
+// // console.log(titleCased);
+
+
+
+function titleCased(){
+  let i = 0;
+  tutorials.forEach((sentence)=>{
+    const words = sentence.split(' ');
+    //console.log(words);
+    let newSentenceArray = words.map((word)=>{
+      return word.charAt(0).toUpperCase() + word.slice(1); 
+    })
+    //console.log(newSentenceArray)
+    const newtitleCasedSentence = newSentenceArray.join(' ');
+    //console.log(newtitleCasedSentence)
+    tutorials[i] = newtitleCasedSentence;
+    i++;
+})
+
+return tutorials;
+
 }
+
+console.log(titleCased());
+
